ff7540a0fe731ccb9725f2cda889dfc507660873
nicholauszoom <nicholaussomi5@gmail.com>
2025-07-06 03:31:25 +0300

    add small change

--- database-adv-script/performance.sql
+++ database-adv-script/performance.sql
@@ -29,4 +29,24 @@ WHERE
     bookings.status = 'confirmed'
     AND payments.status = 'paid';
 
+
+-- Optimized Query using fewer columns and LEFT JOIN
+EXPLAIN ANALYZE
+SELECT 
+    b.id AS booking_id,
+    b.start_date,
+    b.end_date,
+    u.name AS user_name,
+    p.title AS property_title,
+    pay.amount
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.id
+JOIN 
+    properties p ON b.property_id = p.id
+LEFT JOIN 
+    payments pay ON b.id = pay.booking_id;
+
+
 END;
